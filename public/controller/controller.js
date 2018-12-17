5653f2488d4d7743075e63f30884c86e229cf281
Vasiliy Matutin <vmatutin@gmail.com>
2018-12-17 22:12:08 +0300

    HOTFIX: Remove service message

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -12,7 +12,6 @@ socket.on('connection_error', (data) => {
 });
 
 socket.on('joined_room', (data) => {
-    console.log('ugu');
     playerNumber = data.playerNumber;
     $(document).ready(function() {
         $('#main-container').hide();
